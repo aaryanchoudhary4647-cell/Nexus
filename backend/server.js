bfb8e485532eb36768c26fd38b776761973a88c6
aaryanchoudhary4647-cell <aaryanchoudhary4647@gmail.com>
2026-07-02 17:47:02 +0530

    debug deployment issue

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://nexus-frontend-smoky.vercel.app/",
+        origin: "https://nexus-frontend-smoky.vercel.app",
         methods: ["GET", "POST"],
     }
 })
